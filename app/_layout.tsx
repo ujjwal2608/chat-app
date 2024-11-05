e675df1acdb0e4846707e7ab73f91aae7c3b4af7
ujjwal.aggarwal <ujjwal.aggarwal@geekyants.com>
2024-11-05 16:10:50 +0530

    integrated the socket.io for login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,20 @@
+import { ReactNode } from 'react';
 import '../global.css';
-
+import { AuthContextProvider } from './context/AuthContext';
+import { SocketContextProvider } from './context/SocketContext';
 import { Stack } from 'expo-router';
 
+const MainLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <AuthContextProvider>
+      <SocketContextProvider>{children}</SocketContextProvider>
+    </AuthContextProvider>
+  );
+};
 export default function Layout() {
-  return <Stack />;
+  return (
+    <MainLayout>
+      <Stack />
+    </MainLayout>
+  );
 }
