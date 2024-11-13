3511e2da52944a7c6977260f793ce6f0da28fc84
ujjwal.aggarwal <ujjwal.aggarwal@geekyants.com>
2024-11-13 11:43:35 +0530

    made the screen sfor routing in video call

---
+++ app/chat/OutGoingVideocall.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const OutGoingVideocall = () => {
+  return (
+    <View>
+      <Text>OutGoingVideocall</Text>
+    </View>
+  )
+}
+
+export default OutGoingVideocall
+
+const styles = StyleSheet.create({})
---
+++ app/chat/VideoCall.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const VideoCall = () => {
+  return (
+    <View>
+      <Text>VideoCall</Text>
+    </View>
+  )
+}
+
+export default VideoCall
+
+const styles = StyleSheet.create({})
